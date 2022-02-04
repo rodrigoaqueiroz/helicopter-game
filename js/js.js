04addcf7e5d626744cececc6449024469c664406
rodrigoaqueiroz <rodrigoandradequeiroz@gmail.com>
2022-02-04 15:49:20 -0300

    feat: move friendly units

--- js/js.js
+++ js/js.js
@@ -31,6 +31,8 @@ function start() {
 	  movefundo();
     movejogador();
     moveinimigo1();
+    moveinimigo2();
+    moveamigo();
 	};
 
 //Função que movimenta o fundo do jogo
@@ -69,5 +71,22 @@ function start() {
       $("#inimigo1").css("left",694);
       $("#inimigo1").css("top",posicaoY);  
     };
+  };
+
+  function moveinimigo2() {
+    posicaoX = parseInt($("#inimigo2").css("left"));
+    $("#inimigo2").css("left",posicaoX-3);
+      
+    if (posicaoX<=0) {  
+      $("#inimigo2").css("left",775);   
+    };
+  }; 
+  
+  function moveamigo() {	
+    posicaoX = parseInt($("#amigo").css("left"));
+    $("#amigo").css("left",posicaoX+1);       
+    if (posicaoX>906) {   
+      $("#amigo").css("left",0);      
+    }; 
   };
 };
