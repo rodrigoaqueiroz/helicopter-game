8400a329cc4a9a7bee5fac31a5517eae153fd8a2
rodrigoaqueiroz <rodrigoandradequeiroz@gmail.com>
2022-02-04 15:24:22 -0300

    feat: animations

--- js/js.js
+++ js/js.js
@@ -1,10 +1,22 @@
 function start() {
-
 	$("#inicio").hide();
-	
-	$("#fundoGame").append("<div id='jogador'></div>");
-	$("#fundoGame").append("<div id='inimigo1'></div>");
+	$("#fundoGame").append("<div id='jogador' class='anima1'></div>");
+	$("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
 	$("#fundoGame").append("<div id='inimigo2'></div>");
-	$("#fundoGame").append("<div id='amigo'></div>");
+	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
+	
+	var jogo = {}
 
+	//Game Loop
+
+	jogo.timer = setInterval(loop,30);
+	function loop() {
+	  movefundo();
+	}
+
+//Função que movimenta o fundo do jogo
+	function movefundo() {
+  	esquerda = parseInt($("#fundoGame").css("background-position"));
+	  $("#fundoGame").css("background-position",esquerda-1);
+  } 
 }
