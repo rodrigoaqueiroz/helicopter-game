096c295d386a08982e0d23d45600444b3bdf4ad0
rodrigoaqueiroz <rodrigoandradequeiroz@gmail.com>
2022-02-04 17:14:28 -0300

    feat: increasing difficulty

--- js/js.js
+++ js/js.js
@@ -203,6 +203,7 @@ function start() {
   
     if (colisao3.length>0) {		
       pontos += 100;
+      velocidade=velocidade+0.3;
       inimigo1X = parseInt($("#inimigo1").css("left"));
       inimigo1Y = parseInt($("#inimigo1").css("top"));
       explosao1(inimigo1X,inimigo1Y);
